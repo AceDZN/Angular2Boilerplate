131f03be52a06c75657ca9b5a9de9ec1b758a01d
Alex Sindalovsky <design@acedzn.com>
2016-06-05 02:12:25 +0300

    + Added Travis.yml + Added a dummy test * replaced the component selector

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'my-app',
+    selector: 'ng-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
         <p>Hello World!!</p>
